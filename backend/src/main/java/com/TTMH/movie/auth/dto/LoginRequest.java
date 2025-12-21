268ea070c06895d19d10384267ed87f038ddcbbd
minhhuy <minhhuy5929@gmail.com>
2025-12-21 15:19:59 +0700

    add [RegisterRequest, Login Request, AuthResponse]

---
+++ backend/src/main/java/com/TTMH/movie/auth/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.TTMH.movie.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @Email @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
